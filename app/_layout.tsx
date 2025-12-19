6e76cca63648608c91dd5e3bad816a4d6bf116f7
Ashley-Blu <matsekolengashley28@hmail.com>
2025-12-19 11:24:44 +0200

    auth + route protecting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,51 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { getLoggedInUser } from "../storage/authStorage";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Layout() {
+  const router = useRouter();
+  const segments = useSegments();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await getLoggedInUser();
+      const inAuthGroup = segments[0] === "auth";
+
+      if (!user && !inAuthGroup) {
+        router.replace("/auth/login");
+      }
+
+      if (user && inAuthGroup) {
+        router.replace("/notes/work");
+      }
+
+      setLoading(false);
+    };
+
+    checkAuth();
+  }, [segments]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/login" options={{ headerShown: false}} />
-      <Stack.Screen name="auth/register" options={{ headerShown: false }} />
-      <Stack.Screen name="notes/work" options={{ headerShown: false }} />
-      <Stack.Screen name="notes/study" options={{ headerShown: false }} />
-      <Stack.Screen name="notes/personal" options={{ headerShown: false }} />
-      <Stack.Screen name="notes/add" options={{ headerShown: false }} />
-      <Stack.Screen name="notes/edit" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="auth/login" />
+      <Stack.Screen name="auth/register" />
+      <Stack.Screen name="notes/work" />
+      <Stack.Screen name="notes/study" />
+      <Stack.Screen name="notes/personal" />
+      <Stack.Screen name="notes/add" />
+      <Stack.Screen name="notes/edit" />
+      <Stack.Screen name="profile" />
     </Stack>
   );
 }
